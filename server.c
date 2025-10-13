0daaa33a47d639d483b1ee7359400ede9bd4c0ab
jao-07 <joaovecruz@yahoo.com.br>
2025-10-13 16:39:45 -0300

    Continuação do desenvolvimento do arquivo de server, com a adição de listen, accept, e o inicio da troca de dados com o client, com recv e send

--- server.c
+++ server.c
@@ -9,30 +9,31 @@ int create_server_socket(char* protocol){
     else
         server_socket = -1;
 
-    if(server_socket < 0){
-        perror("Erro ao criar o socket de servidor");
-        exit(-1);
-    }
-
     return server_socket;
 }
 
-struct sockaddr_in *create_ipv4_addr(int port){  
-    struct sockaddr_in *address_v4 = malloc(sizeof(struct sockaddr_in));
-    address_v4->sin_family = AF_INET;
-    address_v4->sin_port = htons(port);
-    address_v4->sin_addr.s_addr = INADDR_ANY;
+struct sockaddr *create_server_addr(int port, char* protocol, socklen_t *len){
+    if(!strcmp(protocol, "v4")){
+        struct sockaddr_in *address_v4 = malloc(sizeof(struct sockaddr_in));
+        address_v4->sin_family = AF_INET;
+        address_v4->sin_port = htons(port);
+        address_v4->sin_addr.s_addr = INADDR_ANY;
+        *len = sizeof(*address_v4);
 
-    return address_v4;
-}
+        return (struct sockaddr *)address_v4;
+    }
 
-struct sockaddr_in6 *create_ipv6_addr(int port){  
-    struct sockaddr_in6 *address_v6 = malloc(sizeof(struct sockaddr_in6));
-    address_v6->sin6_family = AF_INET6;
-    address_v6->sin6_port = htons(port);
-    address_v6->sin6_flowinfo = 0; 
-    address_v6->sin6_addr.s6_addr = in6addr_any;
-    address_v6->sin6_scope_id = 0;
+    if(!strcmp(protocol,"v6")){
+        struct sockaddr_in6 *address_v6 = malloc(sizeof(struct sockaddr_in6));
+        address_v6->sin6_family = AF_INET6;
+        address_v6->sin6_port = htons(port);
+        address_v6->sin6_flowinfo = 0; 
+        address_v6->sin6_addr.s6_addr = in6addr_any;
+        address_v6->sin6_scope_id = 0;
+        *len = sizeof(*address_v6);
 
-    return address_v6;
+        return (struct sockaddr *)address_v6;
+    }
+    
+    return NULL;
 }
--- server_main.c
+++ server_main.c
@@ -2,16 +2,76 @@
 
 int main(int argc, char *argv[]){
 
-    int server_socket = create_server_socket(argv[1]);
-
+    int server_socket, client_socket;
+    char* protocol = argv[1];
     int port = atoi(argv[2]);
-    if(port > 1024 && port <= 65535){
-        printf("Porta válida\n");
+
+    struct sockaddr *server_addr;
+    socklen_t server_addrlen;
+
+    char input_buffer[1024] = { 0 };
+    char output_buffer[1024] = { 0 };
+
+    // struct sockadd_in client_addr4;
+    // struct sockadd_in6 client_addr6;
+    // struct sockaddr *client_addr;
+    // socklen_t client_addrlen;
+
+    server_socket = create_server_socket(argv[1]);
+
+    if(server_socket < 0){
+        perror("Erro ao criar o socket de servidor");
+        exit(EXIT_FAILURE);
     }
-    else{
-        printf("Parâmetro de porta inválido!\n");
-        exit(-1);
+    
+    server_addr = create_server_addr(port, protocol, &server_addrlen);
+    if(!server_addr){
+        perror("Erro ao criar o endereço");
+        exit(EXIT_FAILURE);
     }
 
-    if(!strcmp(protocol, "v4")){
+    if(bind(server_socket, server_addr, server_addrlen) < 0){
+        perror("Erro no bind");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_socket, 1) < 0) {
+        perror("Erro no listen");
+        exit(EXIT_FAILURE);
+    }
+
+    // if(!strcmp(protocol, "v4")){
+    //     client_addr = (struct sockaddr *)&client_addr4
+    //     client_addrlen = sizeof(client_addr4)
+    // }
+    // else{
+    //     client_addr = (struct sockaddr *)&client_addr6
+    //     client_addrlen = sizeof(client_addr6)
+    // }
+
+    client_socket = accept(server_socket, NULL, NULL);
+
+    if(client_socket < 0){
+        perror("Erro no accept");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1){
+        memset(input_buffer, 0, sizeof(input_buffer));
+        ssize_t received_bytes = recv(client_socket, input_buffer, sizeof(input_buffer) - 1, 0);
+        if(received_bytes <= 0){
+            printf("Conexão encerrada.");
+            break;
+        }
+
+        input_buffer[received_bytes] = '\0';
+        printf("Mensagem do cliente: %s\n", input_buffer);
+
+        memset(output_buffer, 0, sizeof(output_buffer));
+        send(client_socket, buffer)
+    }
+
+    close(client_socket);
+    close(server_socket);
+
 }
